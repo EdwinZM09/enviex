a9ff00561f5dc9194e956440d05df593fcfa1541
Fer Valdez <f.douzez@gmail.com>
2020-07-01 22:26:27 -0400

    add spring project

---
+++ src/main/java/com/enviex/paqueteria/EnviexApplication.java
@@ -0,0 +1,13 @@
+package com.enviex.paqueteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviex/paqueteria/EnviexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviex.paqueteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
